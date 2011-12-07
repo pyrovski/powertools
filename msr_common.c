d8b43a1cc8a18ebe1173d536f2b9c9d1a145bad1
Barry Rountree <barry.rountree@gmail.com>
2011-12-06 19:42:05 -0800

    Nice little power meter.

--- msr_common.c
+++ msr_common.c
@@ -12,28 +12,46 @@ int msr_debug;
 #include "msr_rapl.h"
 
 uint64_t global_test;
-
+/*
 static void
 spin(uint64_t i){
 	for(global_test=0; global_test<i; global_test++);
 }
+*/
 
 
 
 int
 main(int argc, char **argv){
 	struct power_units units;
-	struct power_info info;
-	struct power_limit limit;
-	double joules, power; 
+	struct power_info info[NUM_DOMAINS];
+	double joules[NUM_DOMAINS]; 
+	struct timeval now;
+	/*
+	struct power_limit limit[NUM_DOMAINS];
+	double power[4]; 
 	uint64_t policy; 
 	int i;
 	struct timeval start, stop;
-	msr_debug=1;
+	*/
+	msr_debug=0;
 	init_msr();
 	get_rapl_power_unit(0, &units);
-	get_power_info(0, PKG_DOMAIN, &info, &units);
-
+	get_power_info(0, PKG_DOMAIN, &info[PKG_DOMAIN], &units);
+	get_energy_status(0, PKG_DOMAIN, &joules[PKG_DOMAIN], &units);
+	get_energy_status(0, PP0_DOMAIN, &joules[PP0_DOMAIN], &units);
+	get_energy_status(0, PP1_DOMAIN, &joules[PP1_DOMAIN], &units);
+	while(1){
+		gettimeofday(&now, NULL);
+		get_energy_status(0, PKG_DOMAIN, &joules[PKG_DOMAIN], &units);
+		get_energy_status(0, PP0_DOMAIN, &joules[PP0_DOMAIN], &units);
+		get_energy_status(0, PP1_DOMAIN, &joules[PP1_DOMAIN], &units);
+		fprintf(stderr, "%0lu.%0lu pkg %15.10lf pp0 %15.10lf pp1 %15.10lf\n", 
+			now.tv_sec, now.tv_usec,
+			joules[PKG_DOMAIN],joules[PP0_DOMAIN],joules[PP1_DOMAIN] );
+		sleep(1);
+	}
+/*	
 	get_energy_status(0, PKG_DOMAIN, &joules, &units);
 	gettimeofday( &start, NULL );
 	sleep(1);
@@ -54,6 +72,7 @@ main(int argc, char **argv){
 
 	get_pkg_power_limit( 0, PKG_DOMAIN, &limit, &units );
 	get_policy( 0, PP0_DOMAIN, &policy );
+*/
 	return 0;
 }
 
