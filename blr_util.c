e622690201c5b25339797eadd8e9adbc668b5c9b
Barry Rountree <rountree4@llnl.gov>
2012-01-09 13:20:47 -0800

    Added safe_mkstemp()

--- blr_util.c
+++ blr_util.c
@@ -1,7 +1,25 @@
+#include <stdlib.h>		// mkstmp
+#include <unistd.h>		// close
 #include "blr_util.h"
 
 double 
 ts_delta(struct timeval *start, struct timeval *stop){
 	return (stop->tv_sec + stop->tv_usec/1000000.0) - (start->tv_sec + start->tv_usec/1000000.0);
+}
+
+FILE *
+safe_mkstmp( int rank, const char* str ){
+	FILE *f;
+	int fd=-1;
+	char filename[1024];
+	sprintf(filename, "%04d_%s_XXXXXX", rank, str);
+	fd=mkstemp(filename);
+	if(fd==-1){
+		fprintf(stderr, "%s::%d  Error opening %s for reading.\n", __FILE__, __LINE__, filename);
+		return (FILE*)-1;
+	}
+	close(fd);
+	f = fopen(filename, "w");
+	return f;
 }
 
--- blr_util.h
+++ blr_util.h
@@ -1,6 +1,8 @@
 #include <sys/time.h>
+#include <stdio.h>
 #ifndef BLR_UTIL_H
 #define BLR_UTIL_H
 double ts_delta(struct timeval *start, struct timeval *stop);
+FILE * safe_mkstmp(int rank, const char *str);
 #endif // BLR_UTIL_H
 
