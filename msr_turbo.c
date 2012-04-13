7062d9c7c6934ead6d8fc48ea0c413f4520d0cc5
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-12 22:53:13 -0700

    (disabled) in case bit 38 of MSR_IA32_MISC_ENABLE is set, clear it to enable control of Turboboost through MSR_IA32_PERF_CTL

--- msr_turbo.c
+++ msr_turbo.c
@@ -8,13 +8,20 @@
 // MSRs common to 062A and 062D.
 
 // setting bit 38 high DISABLES turbo mode. (BIOS ONLY)
-#define MSR_MISC_ENABLE			0x1A0	// aka IA32_MISC_ENABLE
+#define MSR_IA32_MISC_ENABLE            0x000001a0
 
 #define MSR_IA32_PERF_CTL               0x00000199
 
 void
 enable_turbo(int socket){
 	uint64_t val;
+	/*
+	read_msr( socket, MSR_IA32_MISC_ENABLE, &val );
+	// Set bit 38 to 0.
+	val &= ((uint64_t)-1) ^ ((uint64_t)1) << 38;
+	write_msr( socket, MSR_IA32_MISC_ENABLE, val );
+	*/
+
 	read_msr( socket, MSR_IA32_PERF_CTL, &val );
 	// Set bit 32 to 0.
 	val &= ((uint64_t)-1) ^ ((uint64_t)1) << 32;
@@ -25,6 +32,13 @@ enable_turbo(int socket){
 void
 disable_turbo(int socket){
 	uint64_t val;
+	/*
+	read_msr( socket, MSR_IA32_MISC_ENABLE, &val );
+	// Set bit 38 to 0.
+	val &= ((uint64_t)-1) ^ ((uint64_t)1) << 38;
+	write_msr( socket, MSR_IA32_MISC_ENABLE, val );
+	*/
+
 	read_msr( socket, MSR_IA32_PERF_CTL, &val );
 	// Set bit 32 to 1.
 	val |= ((uint64_t)1) << 32;
