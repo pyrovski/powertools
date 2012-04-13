9f60b39fd8a4fca91397751c4d35305b5dd3c75d
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-12 22:22:54 -0700

    use MSR_IA32_PERF_CTL for turboboost control

--- msr_turbo.c
+++ msr_turbo.c
@@ -6,26 +6,28 @@
 #include "msr_turbo.h"
 
 // MSRs common to 062A and 062D.
-#define MSR_MISC_ENABLE			0x1A0	// aka IA32_MISC_ENABLE
-						// setting bit 38 high DISABLES turbo mode.
 
+// setting bit 38 high DISABLES turbo mode. (BIOS ONLY)
+#define MSR_MISC_ENABLE			0x1A0	// aka IA32_MISC_ENABLE
+
+#define MSR_IA32_PERF_CTL               0x00000199
 
 void
-enable_turbo(int cpu){
+enable_turbo(int socket){
 	uint64_t val;
-	read_msr( cpu, MSR_MISC_ENABLE, &val );
-	// Set bit 38 to 0.
-	val &= ((uint64_t)-1) ^ ((uint64_t)1) << 38;
-	write_msr( cpu, MSR_MISC_ENABLE, val );
+	read_msr( socket, MSR_IA32_PERF_CTL, &val );
+	// Set bit 32 to 0.
+	val &= ((uint64_t)-1) ^ ((uint64_t)1) << 32;
+	write_msr( socket, MSR_IA32_PERF_CTL, val );
 }
 
 
 void
-disable_turbo(int cpu){
+disable_turbo(int socket){
 	uint64_t val;
-	read_msr( cpu, MSR_MISC_ENABLE, &val );
-	// Set bit 38 to 1.
-	val |= ((uint64_t)1) << 38;
-	write_msr( cpu, MSR_MISC_ENABLE, val );
+	read_msr( socket, MSR_IA32_PERF_CTL, &val );
+	// Set bit 32 to 1.
+	val |= ((uint64_t)1) << 32;
+	write_msr( socket, MSR_IA32_PERF_CTL, val );
 }
 
