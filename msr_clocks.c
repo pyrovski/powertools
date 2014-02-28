89c7a4acae5546f3eb92e55cc900993b0b3c77d5
Peter Bailey <peter.eldridge.bailey@gmail.com>
2014-02-27 17:38:40 -0700

    update todo list

--- msr_clocks.c
+++ msr_clocks.c
@@ -10,6 +10,7 @@
 #define MSR_IA32_MPERF 0x000000e7
 #define MSR_IA32_APERF 0x000000e8
 
+//!@todo convert to a single read
 void read_aperf_mperf(int socket, uint64_t *aperf, uint64_t *mperf){
 	read_msr( socket, MSR_IA32_MPERF, mperf );
 	read_msr( socket, MSR_IA32_APERF, aperf );
