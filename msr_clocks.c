4097d0e1c84e05903dd5c1ca894f8e50fa3d9ece
Peter Bailey <peter.eldridge.bailey@gmail.com>
2014-12-03 13:59:10 -0700

    debugging

--- msr_clocks.c
+++ msr_clocks.c
@@ -5,7 +5,7 @@
 
 #include "msr_core.h"
 #include "msr_clocks.h"
-/* #include <stdio.h> */
+#include <stdio.h>
 
 #define MSR_IA32_MPERF 0x000000e7
 #define MSR_IA32_APERF 0x000000e8
@@ -14,7 +14,9 @@
 void read_aperf_mperf(int core, uint64_t *aperf, uint64_t *mperf){
 	read_msr( core, MSR_IA32_MPERF, mperf );
 	read_msr( core, MSR_IA32_APERF, aperf );
-	/* fprintf(stderr, "aperf: 0x%llx\tmperf: 0x%llx\n", */
-	/* 	*aperf, *mperf); */
+#ifdef _DEBUG
+	fprintf(stderr, "c%d aperf: 0x%llx\tmperf: 0x%llx\n",
+					core, *aperf, *mperf);
+#endif
 }
 
